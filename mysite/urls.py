2ad7a1a58292d6a05180d7f6e52e04042bc71976
Rajpoudel46 <gaulekta789@gmail.com>
2022-12-28 21:20:43 +0545

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,39 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.urls import include, re_path
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views
+
+
+urlpatterns = [
+    path(r'^admin/',admin.site.urls),
+    re_path(r'',include('blog.urls')),
+    re_path(r'accounts/login/$',views.login,name='login'),
+    re_path(r'accounts/logout/$',views.logout,name='logout',kwargs={'next_page':'/'}),
+
+
+
+
+
+# Calling Class based views
+    # re_path(r'^$', views.CBView.as_view())
+    # re_path(r'^$',views.IndexView.as_view()),
+    # re_path(r'^basic_app/',include('basic_app.urls',namespace='basic_app'))
+
+]
